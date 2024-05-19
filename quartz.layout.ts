e7770a4f20c1ff085b70d92087fb6c54705aae90
Cody <CodyEthanJordan@users.noreply.github.com>
2024-05-18 21:34:29 -0500

    Dont show graph or backlinks

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -21,9 +21,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    //Component.Graph(),
+    //Component.Backlinks(),
   ],
 }
 
